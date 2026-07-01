b7230885285f328b767b80a745c27f5cd59ec7bd
CampusChoo <campuschoo@gmail.com>
2026-07-01 10:17:50 -0400

    new

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -8,6 +8,8 @@ interface ImportMetaEnv {
   readonly VITE_SOCKET_URL?: string;
   readonly VITE_GOOGLE_CLIENT_ID?: string;
   readonly VITE_RECAPTCHA_SITE_KEY?: string;
+  readonly VITE_SUPABASE_URL?: string;
+  readonly VITE_SUPABASE_ANON_KEY?: string;
 }
 
 interface ImportMeta {
